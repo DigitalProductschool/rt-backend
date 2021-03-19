cb3d896fc7fcd214c65bed0a837fdce0bd8cbb20
ntmagda <ntmagda93@gmail.com>
2021-03-19 12:48:41 +0100

    added batch as a parameter to the query

--- models.py
+++ models.py
@@ -4,13 +4,12 @@ from DataTypes.Applicant import Applicant
 
 details_ref = db.collection('batch-details')
 query = QueryType()
-Applicants = []
 
 
 @query.field("applicants")
-def resolve_applicants(_, info):
+def resolve_applicants(_, info, batch_id):
+    Applicants = []
     batches = db.collection('batches')
-    batch_id = 11
     applications = batches.document('batch-'+str(batch_id)).collection('applications')
     participants_details = [doc.to_dict() for doc in applications.stream()]
 
