15e8c8c053cf838a5d0cfc29d9f4a5be0cb7a78e
ntmagda <ntmagda93@gmail.com>
2021-03-22 10:11:39 +0100

    created more advanced schema together with a Document class

--- DataTypes/Applicant.py
+++ DataTypes/Applicant.py
@@ -1,9 +1,27 @@
+from enum import Enum
+# for now Track Enum is not used, because track is represetned as a string in the database - maybe we should change that?
+class Track(Enum):
+    SE = 1
+    AI = 2
+    PM = 3
+    UX = 4
+    AC = 5
+
+class Document():
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+
 class Applicant():
-   def __init__(self, name, batch, email):
+   def __init__(self, name, batch, track, email, consent, coverLetter, cv, scholarship, source):
        self.name = name
        self.batch = batch
+       self.track = track
        self.email = email
+       self.consent = consent
+       self.coverLetter = coverLetter
+       self.cv = Document(**cv)
+       self.scholarship = scholarship
+       self.source = source
 
-
-
-       
--- models.py
+++ models.py
@@ -1,8 +1,7 @@
 from ariadne import QueryType
 from database import db
-from DataTypes.Applicant import Applicant
+from DataTypes.Applicant import Applicant, Document
 
-details_ref = db.collection('batch-details')
 query = QueryType()
 
 
@@ -10,10 +9,19 @@ query = QueryType()
 def resolve_applicants(_, info, batch_id):
     Applicants = []
     batches = db.collection('batches')
-    applications = batches.document('batch-'+str(batch_id)).collection('applications')
-    participants_details = [doc.to_dict() for doc in applications.stream()]
+    applications = batches.document('batch-' + str(batch_id)).collection('applications')
+    applications = [doc.to_dict() for doc in applications.stream()]
 
-    for applicant in participants_details:
-        participant = Applicant(applicant['name'], applicant['batch'], applicant['email'])
-        Applicants.append(participant)
+    for application in applications:
+        applicant = Applicant(application['name'],
+                              application['batch'],
+                              application['track'],
+                              application['email'],
+                              application['consent'],
+                              application['coverLetter'],
+                              application['cv'],
+                              application['scholarship'],
+                              application['source']    
+                              )
+        Applicants.append(applicant)
     return Applicants
