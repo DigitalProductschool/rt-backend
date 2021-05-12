ab073c4ca1b383129ad2f5025dd2712f0283bb7d
bsinoimeri <b.sinoimeri@hotmail.com>
2021-05-12 23:04:29 +0200

    Remove authlib

--- Backend/__init__.py
+++ Backend/__init__.py
@@ -1,15 +1,12 @@
 from flask import Flask
 from Backend.config import Config
-from authlib.integrations.flask_client import OAuth
 from flask_cors import CORS
 
-oauth = OAuth()
 cors = CORS()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
-    oauth.init_app(app)
     cors.init_app(app)
     
     from Backend.GraphQL.routes import graphql
