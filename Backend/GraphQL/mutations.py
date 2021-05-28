2744e361c6aa36340605416efccfd511f3c58d53
ntmagda <ntmagda93@gmail.com>
2021-05-28 14:46:02 +0200

    added mutation to insert rating based on the current loged in user

---
+++ Backend/GraphQL/mutations.py
@@ -0,0 +1,26 @@
+from ariadne import MutationType
+from Backend.database import db
+from Backend.DataTypes.User import User
+from Backend.Authentication.verify_token import get_user_context
+
+
+batch_details = db.collection('batch-details')
+batches = db.collection('batches')
+mutation = MutationType()
+
+
+@mutation.field("rate")
+def resolve_rate(_, info, batch_id, applicant_id, score):
+    current_user = get_user_context(info)
+    current_user = User(12345226755,"Magda", "ntmagda93@gmail.com", "photo")
+    if(current_user):
+        applications = batches.document(
+            'batch-' + str(batch_id)).collection('applications')
+        applicant = applications.document(str(applicant_id))
+        ratings = applicant.get().to_dict()['ratings']
+        ratings[str(current_user.uid)] = score
+        applicant.update({"ratings": ratings})
+        return True
+    else:
+        return False
+
--- Backend/GraphQL/routes.py
+++ Backend/GraphQL/routes.py
@@ -5,6 +5,8 @@ from flask import Flask, request, jsonify, Blueprint
 from ariadne import graphql_sync, make_executable_schema, gql, load_schema_from_path
 from ariadne.constants import PLAYGROUND_HTML
 from Backend.GraphQL.queries import query, ApplicantsQueryResult, BatchesQueryResult, ApplicantDetailsQueryResult
+from Backend.GraphQL.mutations import mutation
+
 from flask_cors import CORS
 from Backend.database import db
 from datetime import timedelta
@@ -16,13 +18,12 @@ batch_details = db.collection('batch-details')
 ###################### GRAPHQL API #######################
 
 type_defs = gql(load_schema_from_path("Backend/GraphQL/schema.graphql"))
-schema = make_executable_schema(type_defs, [query, ApplicantsQueryResult, BatchesQueryResult, ApplicantDetailsQueryResult])
+schema = make_executable_schema(type_defs, [query, mutation,  ApplicantsQueryResult, BatchesQueryResult, ApplicantDetailsQueryResult])
 
 
 @graphql.route("/graphql", methods=["GET"])
 def graphql_playground():
     return PLAYGROUND_HTML, 200
-
 
 
 @graphql.route("/graphql", methods=["POST"])
