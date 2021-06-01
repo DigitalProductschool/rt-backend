7f395955f5add510b18a8d88de7bbbf1e8fc5bfe
ntmagda <ntmagda93@gmail.com>
2021-06-01 16:08:27 +0200

    rate query returns the status of an applicant based on the vote average

--- Backend/DataTypes/Status.py
+++ Backend/DataTypes/Status.py
@@ -1,4 +1,19 @@
+import enum
+
+
+########### STATUS #########
+# NEW - the applicant did not receive sufficient amount of votes
+# NEUTRAL - the applicant is below the threshold
+# PRETTY_COOL - the applicant is above the threshold
+
+
+
+class StatusType(enum.Enum):
+    NEW = 0
+    NEUTRAL = 1
+    PRETTY_COOL = 2
+
 class Status():
-    def __init__(self, code, message):
-        self.code = code
+    def __init__(self, statusType, message):
+        self.code = statusType
         self.message = message
--- Backend/GraphQL/mutations.py
+++ Backend/GraphQL/mutations.py
@@ -2,7 +2,7 @@ from ariadne import MutationType
 from ariadne import UnionType
 
 from Backend.database import db
-from Backend.DataTypes.Status import Status
+from Backend.DataTypes.Status import Status, StatusType
 from Backend.DataTypes.User import User
 
 from Backend.Authentication.verify_token import get_user_context
@@ -14,11 +14,14 @@ batch_details = db.collection('batch-details')
 batches = db.collection('batches')
 mutation = MutationType()
 
+coolnessThreshold = 3.5 # threshold that if exceeded changed an applicant status to PRETTY_COOL
+requiredVotesNo = 2 # required number of votes for the threshold to be changed
+
 
 @mutation.field("rate")
 def resolve_rate(_, info, batch_id, applicant_id, score):
     current_user = get_user_context(info)
-    # current_user = User(666,"Magda", "ntmagda93@gmail.com", "photo")
+    # current_user = User(655525656,"Magda", "ntmagda93@gmail.com", "photo")
     if(current_user):
         applications = batches.document(
             'batch-' + str(batch_id)).collection('applications')
@@ -26,7 +29,17 @@ def resolve_rate(_, info, batch_id, applicant_id, score):
         ratings = applicant.get().to_dict()['ratings']
         ratings[str(current_user.uid)] = score
         applicant.update({"ratings": ratings})
-        return Status(1, "Applicant successfully voted")
+
+        if(len(ratings) > requiredVotesNo):
+            filtered_vals = [v for _, v in ratings.items()]
+            average = sum(filtered_vals) / len(filtered_vals)
+
+            if average > coolnessThreshold:
+                return Status(StatusType.PRETTY_COOL, "user voted succesfully")
+            else:
+                return Status(StatusType.NEUTRAL, "user voted succesfully")
+        else:
+            return  Status(StatusType.NEW, "user voted succesfully")
     else:
         return AuthenticationException(404, "User does not have permissions")
 
