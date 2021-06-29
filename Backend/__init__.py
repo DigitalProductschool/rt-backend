8c97a32666ac3fddfef5a4b299dd657b4147c557
bsinoimeri <b.sinoimeri@hotmail.com>
2021-06-29 12:45:44 +0200

    Added the env for staging & production

--- Backend/__init__.py
+++ Backend/__init__.py
@@ -1,14 +1,12 @@
 from flask import Flask
-from Backend.config import Config
+from Backend.config import config
 from flask_cors import CORS
-
 cors = CORS()
 
-def create_app(config_class=Config):
+def create_app(config_class=config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config)
     cors.init_app(app)
-    
     from Backend.GraphQL.routes import graphql
     from Backend.Authentication.verify_token import authentication
 
--- Backend/config.py
+++ Backend/config.py
@@ -1,11 +1,34 @@
 from datetime import timedelta
 from google.cloud import secretmanager
 from Backend.database import access_secret_version
-
+import os
 
 class Config:
+    DEBUG = False
+    DEVELOPMENT = False
     SECRET_KEY = "hiringtool"
     TRELLO_API_KEY = access_secret_version("trello-api-key")
     TRELLO_API_SECRET = access_secret_version("trello-api-secret")
+
+class ProductionConfig(Config):
     TRELLO_BOARD_ID = access_secret_version("trello-board-id-se")
     TRELLO_NAME = access_secret_version("trello-board-name-se")
+
+
+class StagingConfig(Config):
+    DEBUG = True
+    TRELLO_BOARD_ID = access_secret_version("trello-board-id-se-staging")
+    TRELLO_NAME = access_secret_version("trello-board-name-se-staging")
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    DEVELOPMENT = True
+
+configs = {
+    'production': ProductionConfig,
+    'staging': StagingConfig,
+    'development': DevelopmentConfig,
+}
+
+env_config =  os.environ.get('FLASK_ENV', 'development')
+config = configs[env_config]
