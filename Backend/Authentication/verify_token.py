f04f57b331bb8df07977c110165f4c861d419467
bsinoimeri <b.sinoimeri@hotmail.com>
2021-07-13 19:50:43 +0200

    Allow only our emails

--- Backend/Authentication/verify_token.py
+++ Backend/Authentication/verify_token.py
@@ -15,9 +15,15 @@ def verify_token(id_token):
     except:
         return None
 
+def verify_email(email):
+    white_list = ["bela.sinoimeri@unternehmertum.de", "bela.sinoimeri@dpschool.io", "magda.nowak-trzos@unternehmertum.de","bedo@unternehmertum.de"]
+    if email not in white_list: 
+        return None
+
 def get_user_data(uid):
     try:
         user = auth.get_user(uid)
+        verify_email(user.email)
         current_user = User(
                       user.display_name,
                       user.email,
