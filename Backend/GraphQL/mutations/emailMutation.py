67162763bb4258bf48aaca907e1ffea037b536eb
ntmagda <magda.nowaktrzos@gmail.com>
2021-10-11 15:29:10 +0300

    first version of copying applicant to participant collection on status change to accepted

--- Backend/GraphQL/mutations/emailMutation.py
+++ Backend/GraphQL/mutations/emailMutation.py
@@ -2,7 +2,7 @@ from Backend.Authentication.verify_token import get_user_context
 from Backend.DataTypes.Exceptions.AuthenticationException import AuthenticationException
 from Backend.DataTypes.Exceptions.IncorrectParameterException import IncorrectParameterException
 
-from Backend.GraphQL.shared import mutation, get_applicant_document
+from Backend.GraphQL.shared import mutation, get_applicant_document, get_batch_doc
 from Backend.DataTypes.Status import Status
 from Backend.emails import Emails
 from Backend.tracks import SE, AI, PM, IxD, AC, PMC
@@ -33,10 +33,24 @@ def config_track(track):
         return track
        
 
+
+
+def convert_applicant_to_participant(info, batch_id, applicant_id):
+    get_applicant_document(info, batch_id, applicant_id)
+    _, application_details = get_applicant_document(info, batch_id, applicant_id)
+    batch_doc = get_batch_doc(batch_id)
+    participants = batch_doc.collection('participants')  
+    participants_doc = participants.document(str(applicant_id)) 
+    participants_doc.set(application_details)
+
+
+
+
 @mutation.field("sendEmail")
 def mutation_email(_, info, applicant_id, email_type, applicant_name, applicant_email, track, batch_id):
     current_user = get_user_context(info)
     # current_user = User(123663, "Magda", "ntmagda393@gmail.com", "photo")
+    additional_info = ""
     if(current_user):
         try:
             application, application_details = get_applicant_document(info, batch_id, applicant_id)
@@ -47,12 +61,15 @@ def mutation_email(_, info, applicant_id, email_type, applicant_name, applicant_
             status = config_status(email_type)      
             application.update({'status': status})
             track_class = config_track(track)
+            if status == 'Accepted':
+                convert_applicant_to_participant(info, batch_id, applicant_id)
+                additional_info += ", applicant was marked as a participant"
             if status == 'Documents Sent':
                 # to be refarctored to get Applicant class
                 Emails(email_type, applicant_id, applicant_name, applicant_email, track_class, batch_id, None).send_email_with_attach()
             else:
                 Emails(email_type, applicant_id, applicant_name, applicant_email, track_class, batch_id, None).send_email()
-            return Status(0,'Email was succesfuly sent')
+            return Status(0,'Email was succesfuly sent ' + additional_info)
 
     else:
         return AuthenticationException()
