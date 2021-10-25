106bb32107486fd5ae35f31786c8792244a1cf3d
Daniel Bedö <danielbedo@gmail.com>
2021-10-25 05:10:34 +0200

    firestore mocks

--- Backend/GraphQL/queries/batches.py
+++ Backend/GraphQL/queries/batches.py
@@ -1,10 +1,9 @@
 
 from Backend.Authentication.verify_token import get_user_context
 from Backend.DataTypes.Exceptions.AuthenticationException import AuthenticationException
-
-
 from Backend.GraphQL.shared import query
 from Backend.repositories.batch.FirestoreBatchRepository import FirestoreBatchRepository
+from Backend.database import db
 
 
 @query.field("batches")
@@ -12,7 +11,7 @@ def resolve_batches(_, info, batch_id_list):
     current_user = get_user_context(info)
 
     if current_user:
-        repository = FirestoreBatchRepository()
+        repository = FirestoreBatchRepository(db)
         return repository.list(batch_id_list)
     else:
         return AuthenticationException()
