2e6c30f02b79566bf13c4835e2ebc6ef5dfbcc96
ntmagda <magda.nowaktrzos@gmail.com>
2022-01-12 13:11:55 +0100

    fixed stupid big

--- Backend/GraphQL/queries/teams.py
+++ Backend/GraphQL/queries/teams.py
@@ -8,7 +8,7 @@ from Backend.database import db
 @query.field("teams")
 def resolve_teams(_, info, batch_id_list):
     teamsRepository = FirestoreTeamRepository(db)
-    res = teamsRepository.get(batch_id_list)
+    res = teamsRepository.list(batch_id_list)
     if not res:
         raise GraphQLError(message="Incorrect parameter")
     return res
